e38fd86d445891549e34f8fb2d2c6633fa58dcef
jhavierc <cepeda_357@hotmail.com>
2018-05-02 15:03:52 -0500

    Directives example

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -12,7 +12,7 @@ export class RecipesListComponent implements OnInit {
 
   recipes:Recipe[] =[
     new Recipe('Recipe 1 ','Recipe test 1','https://static01.nyt.com/images/2016/02/16/dining/16COOKING-SALMONWITHLEEKS2/16COOKING-SALMONWITHLEEKS2-articleLarge.jpg'),
-    new Recipe('Recipe 2 ','Recipe test 2','https://media-cdn.tripadvisor.com/media/photo-s/10/de/16/db/photo2jpg.jpg')
+    new Recipe('Recipe 2 ','Recipe test 2','https://media-cdn.tripadvisor.com/media/photo-s/10/de/16/db/photo2jpg.jpg'),
     new Recipe('Recipe 3 ','Recipe test 3','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSg6LFPrc8F3wrXIekm03rmfnlNC-zj5iv96W7YWrTPEbTWO7x')
   ];
 
